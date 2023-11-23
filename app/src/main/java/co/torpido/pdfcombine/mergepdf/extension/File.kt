ba35b3858b01c1a1df9f17859a61db47641960fc
= <bahadiryazici@users.noreply.github.com>
2023-11-23 13:30:31 +0300

    Bug fixed

--- app/src/main/java/co/torpido/pdfcombine/mergepdf/extension/File.kt
+++ app/src/main/java/co/torpido/pdfcombine/mergepdf/extension/File.kt
@@ -1,47 +1,54 @@
 package co.torpido.pdfcombine.mergepdf.extension
 
+import android.content.ContentResolver
 import android.content.Context
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import android.os.ParcelFileDescriptor
+import android.provider.OpenableColumns
+import android.util.Log
+import android.widget.Toast
 import androidx.documentfile.provider.DocumentFile
+import com.tom_roush.pdfbox.pdmodel.PDDocument
+import java.io.FileDescriptor
 import java.io.IOException
+import java.lang.Exception
 
 
-fun Uri.getFileNameAndExtension(): Pair<String, String>? {
-    val path = this.path
-    if (path != null) {
-        val fileName = path.substringAfterLast("/").replace("]","")
-        val extension = fileName.substringAfterLast(".", "")
-        if (extension.isNotEmpty()) {
-            return Pair(fileName, extension)
+
+fun Uri.getFileNameFromUri(contentResolver: ContentResolver): String {
+    var fileName = ""
+    val cursor = contentResolver.query(this, null, null, null, null)
+    cursor?.use {
+        if (it.moveToFirst()) {
+            val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+            if (nameIndex != -1) {
+                fileName = it.getString(nameIndex)
+            }
         }
     }
-    return null
-}
-
-fun Uri.getDocumentFile(context: Context): DocumentFile? {
-    return DocumentFile.fromSingleUri(context, this)
+    return fileName
 }
 
 fun Uri.getPdfPageCount(context: Context): Int {
     try {
-        val parcelFileDescriptor: ParcelFileDescriptor? =
-       context.applicationContext.contentResolver.openFileDescriptor(this, "r")
-
+        val parcelFileDescriptor: ParcelFileDescriptor? = context.applicationContext.contentResolver.openFileDescriptor(this, "r")
         parcelFileDescriptor?.let {
             val pdfRenderer = PdfRenderer(it)
             val pageCount = pdfRenderer.pageCount
             pdfRenderer.close()
             it.close()
-
             return pageCount
         }
     } catch (e: IOException) {
-        e.printStackTrace()
+       e.printStackTrace()
+        Log.e("PdfPageCount", "Hata", e)
     }
     return -1
 }
 
+fun Uri.getDocumentFile(context: Context): DocumentFile? {
+    return DocumentFile.fromSingleUri(context, this)
+}
 
 
